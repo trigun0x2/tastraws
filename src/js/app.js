5077100f0230710122fa202eeda6661940f60cdd
Jeffrey Tong <trigun0x2@gmail.com>
2016-01-08 10:31:23 -0800

    initial working version

--- src/js/app.js
+++ src/js/app.js
@@ -1,47 +1,19 @@
 var TA = window.TA;
 
-class TwitchTrivia {
-
+class Strawpoll {
   constructor() {
-    let triviaAPI = "http://jservice.io/api/random";
-    let ans = null;
-    let triviaState = false;
-
     TA.twitch.chat.on('say', (data) => {
-      console.log(data);
-      if (data.message == '!trivia' && !triviaState){
-        $.get(triviaAPI, (trivia) => {
-          let question = trivia[0].question;
-          console.log(question);
-          TA.twitch.chat.say(question);
-          $(".text-area").text(question);
-          $(".trivia-cont").fadeIn();
-          ans = trivia[0].answer;
-          console.warn(ans);
-          triviaState = true;
-        });
-      }else if (this.match_ans(data.message, ans)){
-        TA.twitch.chat.say(data.from + " has answered correctly!");
-        $(".question-cont").fadeOut(() => {
-          $(".winner-name").text(data.from);
-          $(".winner-cont").fadeIn();
-        });
-        setTimeout(() => $(".question-cont").fadeOut());
-        triviaState = false;
+      // This should also check to see if the poster is a mod/owner
+      if (data.match(/!strawpoll http:\/\/strawpoll.me\//)){
+        $("#strawpreview").attr('src', data.replace("!strawpoll ","") + "/r");
+        $("#strawpreview").fadeIn();
+      }
+
+      if (data.message == "!strawpoll off"){
+        $("#strawpreview").fadeOut();
       }
     });
-  };
-
-  // Checks if the answer is correct
-  match_ans(guess, answer) {
-   var score = answer.score(guess);
-   console.log(score);
-   return score > 0.7;
-  }
-  debug() {
-    triviaState = false;
-    TA.twitch.chat.emit('say', {message: "!trivia"});
   }
 }
 
-var trivia = new TwitchTrivia();
+var trivia = new Strawpoll();
