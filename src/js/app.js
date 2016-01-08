f70d0ea0e4086f56e77f40330d3a5cc4c4640976
Jeffrey Tong <trigun0x2@gmail.com>
2016-01-08 11:57:00 -0800

    updated to get stream owner

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,11 @@ var TA = window.TA;
 
 class Strawpoll {
   constructor() {
+    this.owner;
+    TA.api.get('/user').then((user) => {
+      this.owner = user.accounts.twitch.metadata.name;
+      console.log(this.owner);
+    });
     TA.twitch.chat.on('say', (data) => {
       // This should also check to see if the poster is a mod/owner
       if (data.message.match(/!strawpoll http:\/\/strawpoll.me\//)){
